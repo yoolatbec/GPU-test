49c7089dd80113970c545dae8d7c1b68d27224ab
yoolatbec <yoolatbec@outlook.com>
2022-09-19 23:17:28 +0800

    remove original files

--- original/gpu-app-collection/src/cuda/proxy-apps-doe/comd/clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-make clean
-rm -f CoMDCUDA
-rm -f *.o
-rm -f utils/*.o
-rm -rf *.so *.vir *.os *.s *.ccbak *.ptx
--- original/gpu-app-collection/src/cuda/proxy-apps-doe/xsbench/runScriptExample.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source /home/ascudiero/scratch/research/einstein/software/o-xterm.bash
-
-export CUDA_USE_HOST_FE=1
-export CUDA_ECX_ARGS="--stats 2"
-
-/home/ascudiero/XSBench-ECX/XSBench 12 small
-
-
